0f137cf8213876a897fbe18def2d013fcbe9703b
Stuart O. Anderson <emblem@gmail.com>
2012-11-10 00:37:16 -0500

    Use the proximity sensor to turn of the screen during calls
    We use the IR proximity sensor, if available, to turn the screen off
    while in-call, if it appears the user's face is close to the screen.
    
    This also fixes an exception that occurred on pre JB devices that did
    not include the isKeyguardLocked() method.
    
    Next step is to use the accelerometer to only enable proximity wakelock
    when the phone is in a 'phone-like' orientation.  (PhoneApp does this)

--- src/org/thoughtcrime/redphone/call/LockManager.java
+++ src/org/thoughtcrime/redphone/call/LockManager.java
@@ -3,10 +3,12 @@ package org.thoughtcrime.redphone.call;
 import android.app.KeyguardManager;
 import android.content.Context;
 import android.net.wifi.WifiManager;
-import android.nfc.Tag;
+import android.os.Build;
 import android.os.PowerManager;
 import android.util.Log;
-import org.thoughtcrime.redphone.RedPhone;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Maintains wake lock state.
@@ -17,11 +19,17 @@ public class LockManager {
   private final Context context;
   private final PowerManager.WakeLock fullLock;
   private final PowerManager.WakeLock partialLock;
+  private final PowerManager.WakeLock proximityLock;
   private final KeyguardManager.KeyguardLock keyGuardLock;
   private final KeyguardManager km;
   private final WifiManager.WifiLock wifiLock;
 
+  private final Method wakelockParameterizedRelease;
+
   private boolean keyguardDisabled;
+
+  private static final int PROXIMITY_SCREEN_OFF_WAKE_LOCK = 32;
+  private static final int WAIT_FOR_PROXIMITY_NEGATIVE = 1;
 
   public enum PhoneState {
     IDLE,
@@ -33,7 +41,8 @@ public class LockManager {
   private enum LockState {
     FULL,
     PARTIAL,
-    SLEEP
+    SLEEP,
+    IN_CALL
   }
 
   public LockManager(Context context) {
@@ -42,6 +51,15 @@ public class LockManager {
     PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
     fullLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "RedPhone Full");
     partialLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "RedPhone Partial");
+    proximityLock = pm.newWakeLock(PROXIMITY_SCREEN_OFF_WAKE_LOCK, "RedPhone Incall");
+
+    Method maybeRelease = null;
+    try {
+      maybeRelease = proximityLock.getClass().getDeclaredMethod("release", Integer.TYPE);
+    } catch (NoSuchMethodException e) {
+      Log.d("LockManager", "Parameterized WakeLock release not available on this device.");
+    }
+    wakelockParameterizedRelease = maybeRelease;
 
     km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
     keyGuardLock = km.newKeyguardLock("RedPhone KeyGuard");
@@ -51,6 +69,7 @@ public class LockManager {
 
     fullLock.setReferenceCounted(false);
     partialLock.setReferenceCounted(false);
+    proximityLock.setReferenceCounted(false);
     wifiLock.setReferenceCounted(false);
   }
 
@@ -70,7 +89,7 @@ public class LockManager {
         break;
       case IN_CALL:
         //TODO(Stuart Anderson): Use proximity wake mode during calls.
-        setLockState(LockState.PARTIAL);
+        setLockState(LockState.IN_CALL);
         disableKeyguard();
         break;
     }
@@ -82,16 +101,25 @@ public class LockManager {
         fullLock.acquire();
         partialLock.acquire();
         wifiLock.acquire();
+        releaseProximityLock();
         break;
       case PARTIAL:
         partialLock.acquire();
         wifiLock.acquire();
         fullLock.release();
+        releaseProximityLock();
         break;
       case SLEEP:
         fullLock.release();
         partialLock.release();
         wifiLock.release();
+        releaseProximityLock();
+        break;
+      case IN_CALL:
+        partialLock.acquire();
+        proximityLock.acquire();
+        wifiLock.acquire();
+        fullLock.release();
         break;
       default:
         throw new IllegalArgumentException("Unhandled Mode: " + newState);
@@ -99,8 +127,26 @@ public class LockManager {
     Log.d("LockManager", "Entered Lock State: " + newState);
   }
 
+  private void releaseProximityLock() {
+    boolean released = false;
+    if (wakelockParameterizedRelease != null) {
+      try {
+        wakelockParameterizedRelease.invoke(proximityLock, new Integer(WAIT_FOR_PROXIMITY_NEGATIVE));
+        released = true;
+      } catch (IllegalAccessException e) {
+        Log.d("LockManager", "Failed to invoke release method", e);
+      } catch (InvocationTargetException e) {
+        Log.d("LockManager", "Failed to invoke release method", e);
+      }
+    }
+
+    if(!released) {
+      proximityLock.release();
+    }
+  }
+
   private void disableKeyguard() {
-    if(km.isKeyguardLocked()) {
+    if(keyguardLocked()) {
       keyGuardLock.disableKeyguard();
       keyguardDisabled = true;
     }
@@ -111,5 +157,12 @@ public class LockManager {
       keyGuardLock.reenableKeyguard();
       keyguardDisabled = false;
     }
+  }
+
+  private boolean keyguardLocked() {
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+      return km.isKeyguardLocked();
+    }
+    return true;
   }
 }
