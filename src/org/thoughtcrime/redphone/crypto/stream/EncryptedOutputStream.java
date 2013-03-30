dc4a9e18d459986dd4b86eaf3c2d438653c2d574
Stuart O. Anderson <emblem@gmail.com>
2013-03-29 14:54:07 -1000

    Added EOS

---
+++ src/org/thoughtcrime/redphone/crypto/stream/EncryptedOutputStream.java
@@ -0,0 +1,11 @@
+package org.thoughtcrime.redphone.crypto.stream;
+
+import java.io.OutputStream;
+
+/**
+ * Writes an encrypted stream.
+ */
+public class EncryptedOutputStream implements OutputStream {
+
+
+}
